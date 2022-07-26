0957dfd912e9da85a06c29d850508b74607a8d77
fullstackdev116 <tatianadmitrenko116@outlook.com>
2022-07-26 10:26:21 -0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to My Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
